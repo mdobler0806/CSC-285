fb0a76a147587ac1767b87d50b2094cdd34440cb
mdobler0806 <144255167+mdobler0806@users.noreply.github.com>
2024-02-24 12:40:18 -0500

    Started HW 3

---
+++ HW3/src/main/java/edu/bhcc/Logan.java
@@ -0,0 +1,10 @@
+package edu.bhcc;
+
+/****************************
+ *  @Author : Matthew Dobler"
+ *  @Date    : 2/23/24"
+ *  @File   : Logan"
+ *  @Class   : CSC-"
+ *****************************/
+public class Logan {
+}
