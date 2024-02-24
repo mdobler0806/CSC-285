6d724d2413fbac2f8a3b8ec8c85c1ddaad4d3662
mdobler0806 <144255167+mdobler0806@users.noreply.github.com>
2024-02-24 12:39:41 -0500

    Class coding

---
+++ ClassFeb23/src/main/java/edu/bhcc/Main.java
@@ -0,0 +1,13 @@
+package edu.bhcc;
+
+/**
+ * @Author : Matthew Dobler",
+ * @Date : 2/23/24",
+ * @File : ${NAME}",
+ * Class   : CSC-242",
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
