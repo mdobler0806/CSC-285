5e16477afacc44584d18342d86f5c83bfe9ba206
mdobler0806 <144255167+mdobler0806@users.noreply.github.com>
2024-04-15 16:32:35 -0400

    Update

---
+++ ClassMar292024/src/main/java/edu/bhcc/Main.java
@@ -0,0 +1,14 @@
+package edu.bhcc;
+
+/****************************
+ *  @Author : Matthew Dobler"
+ *  @Date    : 3/29/24"
+ *  @File   : ${NAME}"
+ *  @Class   : CSC-"
+ *****************************/
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
